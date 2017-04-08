bf2d80850504086a9096e2db70a2bd5782c397f9
Alexis Martin <alexis.yj@gmail.com>
2017-04-08 19:08:12 +0200

    Change website and API for Amplified DLC

--- API/randomizer-server.js
+++ API/randomizer-server.js
@@ -2,12 +2,13 @@ var express = require('express');
 var app = express();
 var randomizer = require('../randomizer');
 
-app.get("/randomizer/+*", function(req, res) {
-	var seed = req.params[0];
+app.get("/randomizer/:seed/:amplified", function(req, res) {
+	var seed = req.params.seed,
+		original = !req.params.amplified;
 	res.setHeader('Content-Type', 'text/xml');
 	res.setHeader('Content-Disposition', 'attachment; filename="necrodancer.xml"');
 	res.status(200);
-	randomizer({seed: seed}).then(function(xml) {
+	randomizer({seed: seed == 'random' ? '' : seed, original: original }).then(function(xml) {
 		res.send(xml);
 	});
 });
